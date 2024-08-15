a98eb4b2023cb84125ce3581d482ec86b75b61a2
sebastian <sebastian@sebastians-Mac-mini.local>
2024-08-15 18:02:42 +0300

    new project 3

---
+++ lesson3/SceneDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  SceneDelegate.swift
+//  lesson3
+//
+//  Created by sebastian on 15.08.24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard scene as? UIWindowScene != nil else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
+
---
+++ lesson3/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  lesson3
+//
+//  Created by sebastian on 15.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("стирай этот метод и пиши код сюда")
+    }
+}
